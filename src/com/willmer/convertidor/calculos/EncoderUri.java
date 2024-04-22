4c495690611d36f59c017352a126dffc251742e0
willmermolina <molina.domotica@gmail.com>
2024-04-21 21:35:35 -0400

    Initial commit

--- src/com/willmer/convertidor/calculos/EncoderUri.java
+++ src/com/willmer/convertidor/calculos/EncoderUri.java
@@ -1,4 +1,16 @@
 package com.willmer.convertidor.calculos;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class EncoderUri {
+    public static String encodeUri(String givenUri){
+        String encodeUri = givenUri;
+        try {
+            encodeUri = URLEncoder.encode(givenUri, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encodeUri;
+    }
 }
